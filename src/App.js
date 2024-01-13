f26391b296172db340fd8c9ab302397d54b441d6
Param-Veer-Singh <paramveersinghchampawat@gmail.com>
2024-01-13 17:54:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState('');
+  const [message, setMessage] = useState('');
+  const [messageClass, setMessageClass] = useState('');
+
+  const handleNumber1Change = (e) => {
+    setNumber1(e.target.value);
+  };
+
+  const handleNumber2Change = (e) => {
+    setNumber2(e.target.value);
+  };
+
+  const handleCalculate = (opr) => {
+    const num1 = parseFloat(number1);
+    const num2 = parseFloat(number2);
+
+    if (!isNaN(num1) && !isNaN(num2)) {
+      let calculatedResult = 0;
+      if(opr == '+'){
+        calculatedResult = num1 + num2;
+      }
+      else if(opr == '-'){
+        calculatedResult = num1 - num2;
+      }
+      else if(opr == '*'){
+        calculatedResult = num1 * num2;
+      }
+      else if(opr == '/'){
+        calculatedResult = num1 / num2;
+      }
+      setMessage('Success !')
+      setMessageClass('success')
+      setResult(`Result: ${calculatedResult}`);
+    } else {
+      setMessage('Error !')
+      setMessageClass('error')
+      setResult('Please enter valid numbers');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >React Calculator</h1>
+      <div className='numbox'>
+        <label></label>
+        <input type="text" value={number1} onChange={handleNumber1Change} placeholder='Num 1'/>
+      </div>
+      <div className='numbox'>
+        <label></label>
+        <input type="text" value={number2} onChange={handleNumber2Change} placeholder='Num 2'/>
+      </div>
+      <div>
+        <button onClick={() => handleCalculate('+')}>+</button>
+        <button onClick={() => handleCalculate('-')}>-</button>
+        <button onClick={() => handleCalculate('*')}>*</button>
+        <button onClick={() => handleCalculate('/')}>/</button>
+      </div>
+      <div className={messageClass}>{message}</div>
+      <div>{result}</div>
     </div>
   );
 }
